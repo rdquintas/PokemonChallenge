4f184759b033d46a13af4e160903c7aedeb4cb77
Ricardo Quintas <rqcenter-web@yahoo.com>
2021-11-23 09:46:12 +0000

    frontend com Express

--- web.js
+++ web.js
@@ -1,14 +1,23 @@
 const App = require('./src/app');
-
+const path = require('path');
 var express = require("express");
 var appExpress = express();
 
 appExpress.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on http://localhost:3000/");
 });
 
 appExpress.get('/gridapi/:sCoordenadas', (req, res) => {
     var oApp = new App();
     oApp.init(req.params.sCoordenadas);
-    res.json(oApp.aGrid);
+    var obj = {
+        aGrid: oApp.aGrid,
+        iPokemonsAcumulados: oApp.iPokemonsAcumulados
+    }
+    res.json(obj);
+});
+
+
+appExpress.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
